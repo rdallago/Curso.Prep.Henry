1205954b7278f69b9b404462e7428d51d82ee197
RDL <ricardodallago@gmail.com>
2021-11-10 12:15:35 -0300

    finish framework 6

---
+++ 07-JS-VI/homework/explicacion_solicitada.md
@@ -0,0 +1,7 @@
+# Homework: Javascript VI
+
+## Instrucciones
+---
+1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+* Funciones Callback: Es una función que se pasa como argumento a otra función. Estas funciones se denominan callbacks. Estas funciones pueden llamarse en cualquier momento y pasar argumentos dentro de la función. Pronto descubriremos por qué las devoluciones de llamada son tan importantes para Javascript. La convención es usar cb como argumento para la variable que se usará de callback.
--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,37 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+
+String.prototype.capitalize=function(){
+  return this.charAt(0).toUpperCase()+ this.slice(1)}
+
+
+var arrayNombre=[nombre];
+var nuevoNombre=arrayNombre.map(function(elemento,indice){
+                             return elemento.capitalize();
+                             })
+
+return nuevoNombre[0];
+
+
+
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+
+return cb(this);
+
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+return cb(n1,n2,cb);
+
 }
 
 function sumarArray(numeros, cb) {
@@ -22,25 +42,56 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+
+
+var suma=numeros.reduce(function(acumulador,elemento){
+
+  return acumulador+elemento;},0);
+
+cb(suma);
+
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-}
+
+
+array.forEach(function(elemento,indice){
+
+  cb(elemento);});}
+
+
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+
+  var newArray=[]
+
+  array.forEach(function(elemento,indice){
+
+
+
+         newArray[indice]=cb(elemento);});
+
+  return newArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+ var newArray=[];
+  for(let i=0;i<array.length;i++){
+  if(array[i][0]==='a')
+    
+    newArray.push(array[i]);}
+
+  return newArray;
 }
 
 // No modificar nada debajo de esta línea
